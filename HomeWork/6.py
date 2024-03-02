1723788e2f88ff5d0e84d1eb1100a897986f041e
Nikitio <n1369387@gmail.com>
2024-03-02 11:29:13 +0300

    commit

--- HomeWork/6.py
+++ HomeWork/6.py
@@ -5,9 +5,8 @@ def draw_star(n):
 
     # Установить начальное положение и направление
     turtle.penup()
-    turtle.goto(0, -100)
+    turtle.goto(0, 0)  # Изменено положение начала рисования звезды
     turtle.pendown()
-    turtle.setheading(0)
 
     # Нарисовать лучи звезды
     for i in range(n):
@@ -15,12 +14,7 @@ def draw_star(n):
         turtle.left(180 - 180 / n)
 
 # Нарисовать звезду с 5 концами
-draw_star(5)
-
-# Нарисовать звезду с 7 концами
-draw_star(7)
-
-# Нарисовать звезду с 9 концами
 draw_star(9)
+
 
 turtle.done()
