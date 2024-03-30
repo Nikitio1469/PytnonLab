638e31e04f0b78be466a63ccfabdf9190a3f175d
Nikitio <150508543+Nikitio1469@users.noreply.github.com>
2024-03-30 12:12:22 +0300

    9.py

--- HomeWork/9.py
+++ HomeWork/9.py
@@ -1,5 +1,13 @@
+import random
+
 studentList = ["Вася", "Петя"]
 studentGrades = {"Вася": [], "Петя": []}
+
+# Добавление еще 5 имен и оценок
+for _ in range(3):
+    newStudent = input("Введите имя студента: ")
+    studentList.append(newStudent)
+    studentGrades[newStudent] = []
 
 while True:
     answer = int(input("выберите действие\n"
@@ -9,31 +17,44 @@ while True:
                        "4-посмотреть весь список студентов\n"
                        "5-добавить оценку студенту\n"
                        "6-посмотреть оценки студента\n"
+                       "7-посмотреть список по оценкам\n"
+                       "8-добавить рандомные оценки студентам\n"
                        "0-выйти из программы\n"))
-    if answer not in [1, 2, 3, 4, 5, 6, 0]:
+
+    if answer not in [1, 2, 3, 4, 5, 6, 7, 8, 0]:
         print("такой команды нет")
         continue
+
+    elif answer == 8:
+        for student in studentGrades:
+            random_grades = [random.randint(1, 5) for _ in range(3)]  # Генерация 3 рандомных оценок от 1 до 10 для каждого студента
+            studentGrades[student] = random_grades
+    
     elif answer == 1:
-        newStudent = input("введите имя студента: ")
+        newStudent = input("Введите имя студента: ")
         studentList.append(newStudent)
         studentGrades[newStudent] = []
+    
     elif answer == 2:
-        delNumber = int(input("введите номер студента для удаления: "))
+        delNumber = int(input("Введите номер студента для удаления: "))
         if delNumber < len(studentList):
             del studentGrades[studentList[delNumber]]
             studentList.pop(delNumber)
         else:
             print("Неверный номер студента.")
+    
     elif answer == 3:
-        delStudent = input("введите имя студента для удаления: ")
+        delStudent = input("Введите имя студента для удаления: ")
         if delStudent in studentList:
             del studentGrades[delStudent]
             studentList.remove(delStudent)
             print("Студент", delStudent, "удален из списка.")
         else:
             print("Студента", delStudent, "нет в списке.")
+    
     elif answer == 4:
         print(studentList)
+    
     elif answer == 5:
         studentName = input("Введите имя студента: ")
         if studentName in studentList:
@@ -41,11 +62,18 @@ while True:
             studentGrades[studentName].append(grade)
         else:
             print("Студента", studentName, "нет в списке.")
+    
     elif answer == 6:
         studentName = input("Введите имя студента, чьи оценки вы хотите посмотреть: ")
         if studentName in studentList:
             print("Оценки студента {}: {}".format(studentName, studentGrades[studentName]))
         else:
             print("Студента", studentName, "нет в списке.")
+
+    elif answer == 7:
+        print("Список по оценкам:")
+        for student in studentGrades:
+            print(student, ": ", studentGrades[student])
+    
     elif answer == 0:
         break
